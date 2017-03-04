532db10323d4ee1639b0494a4b7b4eecefaaa7de
Julie Lee <lhz_julie@163.com>
2017-03-04 22:50:26 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $.get('file.json', function(data) {
-    var str='';
-    $.each(data, function(i, v) {
-      str += '<div class="img-box"><h4>' + v.title + '</h4><img src="img/' + v.url + '"></img>';
-    });
-    $('#container').html(str);
-  });
-});
