2b19879263b7fd04b41230950eeedb0421c65117
Julie <lihaizhu@gstianfu.com>
2017-07-23 20:25:46 +0800

    style

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(function() {
   $.get('file.json', function(data) {
     var str='';
     $.each(data, function(i, v) {
-      str += '<div class="img-box"><h4>' + v.title + '</h4><img src="img/' + v.url + '"></img>';
+      str += '<div class="media"><h4>' + v.title + '</h4><img src="img/' + v.url + '"></img>';
     });
     $('#poster').html(str);
   });
